5194609e0340f438a08c23e6af88f7a69b83c51e
tomas1978 <tomas.nilsson78@gmail.com>
2020-05-18 08:39:32 +0200

    First Commit
    Kastar 1000 tärningar och skriver utfallet

---
+++ DiceSimulation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DiceSimulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nyttUtfall;
+            int[] utfall = new int[6];
+            Console.WriteLine("Antal tärningskast: 1000");
+            Random rand = new Random();
+            for(int i=0;i<1000;i++)
+            {
+                nyttUtfall = rand.Next(0, 6);
+                utfall[nyttUtfall]++;
+            }
+            for(int i=0;i<6;i++)
+            {
+                Console.Write("Antal " + (i + 1) + ": ");
+                Console.WriteLine(utfall[i]);
+            }
+        }
+    }
+}
